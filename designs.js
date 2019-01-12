8aa24ceee2d21235f718ea8ee150179e7e5d7070
Abdullah <aarokan@gmail.com>
2019-01-12 14:10:29 +0300

    Define the variables

--- designs.js
+++ designs.js
@@ -1,7 +1,21 @@
 // Select color input
+const colorInput = document.getElementById('colorPicker');
+
 // Select size input
+const heightInput = document.getElementById('inputHeight');
+const widthInput = document.getElementById('inputWidth');
+const submitButton = document.querySelector('form').lastElementChild;
+
+// Select the canvas able
+const canvasTable = document.querySelector('#pixelCanvas');
+
+// Define variables to store the selected color, height and width
+let colorValue = colorInput.value;
+let heightValue = heightInput.value;
+let widthValue = widthInput.value;
 
 // When size is submitted by the user, call makeGrid()
+
 
 function makeGrid() {
 
